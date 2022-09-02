d544ca25c792536aa1be80dec00b861fe9c9aaf2
dhanushk2105 <dhanushk2105@gmail.com>
2022-09-03 04:23:01 +0530

    Changed config for Deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const cors = require("cors")
 
 
-const router = require("./routes/users.js");
+
 const superadmin = require("./routes/superadmin.js");
 const roles = require("./routes/roles.js");
 const jymplans = require("./routes/jymplans.js");
@@ -20,7 +20,7 @@ const { sequelize } = require("./models");
   app.use(bodyParser.json());
   app.use(cors());
 
-  app.use("/api/",router);
+
   app.use("/superadmin/",superadmin);
   app.use("/superadmin/roles",roles);
   app.use("/superadmin/jymplans",jymplans);
@@ -33,8 +33,8 @@ const { sequelize } = require("./models");
 
 app.listen({ port: port }, async () => {
 
-  await sequelize.sync({ force: true });
-  console.log("Database synced!");
-  // await sequelize.authenticate();
-  // console.log("Database Connected!");
+  // await sequelize.sync({ force: true });
+  // console.log("Database synced!");
+  await sequelize.authenticate();
+  console.log("Database Connected!");
 });
