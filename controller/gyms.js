ac35a9bd1aa14d8651e0f77bfe810e3e36d029c8
dhanushk2105 <dhanushk2105@gmail.com>
2022-09-08 11:34:29 +0530

    Application error

--- controller/gyms.js
+++ controller/gyms.js
@@ -1,21 +1,26 @@
-const {registerGym, gymLogin, getAllGymData, deleteGymData, updateGymData} = require("../services/gyms")
+const {
+  registerGym,
+  gymLogin,
+  getAllGymData,
+  deleteGymData,
+  updateGymData,
+} = require("../services/gyms");
 
 module.exports.signUpGym = (req, res) => {
-  return registerGym(req, res)
+  return registerGym(req, res);
 };
 
 module.exports.loginGym = (req, res) => {
-  return gymLogin(req, res)
+  return gymLogin(req, res);
 };
 
 module.exports.allAccountsGym = (req, res) => {
-  return getAllGymData(req, res)
+  return getAllGymData(req, res);
 };
 
 module.exports.deleteGym = (req, res) => {
   return deleteGymData(req, res);
 };
-
 
 module.exports.updateGym = (req, res) => {
   return updateGymData(req, res);
